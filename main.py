bd84ebae0b28e28bf074549ae18ded5e1cce858b
Anna Ketre <anna.ketre@miller-insurance.com>
2024-02-08 11:49:33 +0000

    dictionary created

--- main.py
+++ main.py
@@ -2,36 +2,30 @@ import yfinance as yf
 import requests
 from prettyprinter import pprint
  
-# Disable SSL certificate verification globally
+
 requests.packages.urllib3.disable_warnings()
  
-# Create a session with SSL verification disabled
 session = requests.Session()
 session.verify = False
  
-company_name = input("Enter the company name here: ")
-# Set the session for yfinance Ticker object
+symbol_input = yf.Ticker(input("enter company symbol: "), session=session)
 
+info_keys = ['longName', 'industry', 'country', 'state', 'fullTimeEmployees', 'marketCap', 'exchange', 'totalRevenue', 'heldPercentInsiders']
 
-def companyname_to_symbol(company_name):
-    return yf.Ticker(company_name).info['symbol']
+info = {}
+for key in info_keys:
+    info[key] = symbol_input.info.get(key, None)
 
-msft = yf.Ticker(companyname_to_symbol(company_name), session=session)
- 
-# Now you can make requests using yfinance without SSL verification
-# pprint(msft.info)
-pprint(companyname_to_symbol(company_name))
+pprint(info)
 
+# longName = symbol_input.info['longName']
+# industry = symbol_input.info['industry']
+# domicile_country = symbol_input.info['country']
+# domicile_state = symbol_input.info['state']
+# headcount = symbol_input.info['fullTimeEmployees']
+# market_cap = symbol_input.info['marketCap']
+# exchange = symbol_input.info['exchange']
+# revenue = symbol_input.info['totalRevenue']
+# retention = symbol_input.info['heldPercentInsiders']
 
-field_name = input("Enter the field you want to get: ").lower
-
-def get_field(ticker):
-    field_info = ticker.info.get(field_name)
-    if field_info:
-        print(f"{field_name}: {field_info}")
-    else:
-        print("Field not found for", ticker)
-
-
-#call function to get industry
-get_field(msft)
+# print(longName, industry, domicile_country, domicile_state, headcount, market_cap, exchange, revenue, retention)
